e12b45cba9844f07c331aacd9edf2be52f50dc6a
penserbjorne <penserbjorne@users.noreply.github.com>
2016-12-07 06:04:54 -0600

    Se añade metodo de solucion, implementacion virtual, codigo de arduino.

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -1,67 +1,60 @@
 #include <Arduino.h>
-//**************************************************************//
-//  Name    : shiftOutCode, Hello World                         //
-//  Author  : Carlyn Maw,Tom Igoe                               //
-//  Date    : 25 Oct, 2006                                      //
-//  Version : 1.0                                               //
-//  Notes   : Code for using a 74HC595 Shift Register           //
-//          : to count from 0 to 255                            //
-//****************************************************************
 
-////Pin connected to DS of 74HC595
-int dataPin = 9;   // Serial Data,  MOSI
+/* 
+ *  Basado en el codigo "shiftOutCode, Hello World" de Carlyn Maw y 
+ *  Tom Igoe como ejemplo en el sitio oficial de Arduino.
+*/
 
-//Pin connected to SH_CP of 74HC595
+int dataPin = 9;    // Serial Data,  MOSI
 int clockPin = 10;  // Clock para el serial,  SCK[1]
-
-int dCLK = 11;   //  Clock general,  DCLK[2]
-
-//Pin connected to ST_CP of 74HC595
-int latchPin = 12;   // Clock para los FF,   LOAD[3]
-
-int clrFF = 13;   // Limpia los FF,  RESET[4]
-
-//Data Array
-byte dataArray[4];
-//Byte holder
-byte data;
+int dCLK = 11;      //  Clock general,  DCLK[2]
+int latchPin = 12;  // Clock para los FF,   LOAD[3]
+int clrFF = 13;     // Limpia los FF,  RESET[4]
+byte dataArray[4];  // Data Array
+byte data;          // Byte holder
 
 void setup() {
-    //set pins to output because they are addressed in the main loop
+    // Indicamos los pines como salida
     pinMode(latchPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
     pinMode(clrFF, OUTPUT);
     pinMode(dCLK, OUTPUT);
 
-    //Toogler Program
+    // Programa que se enviara al FPGA
     dataArray[0] = 0x00;
     dataArray[1] = 0xff;
     dataArray[2] = 0x1c;
     dataArray[3] = 0x76;
 
+     // Limpiamos los FF (clear)
     digitalWrite(clrFF, HIGH);
-    //count up routine
+    // Enviaremos cuatro bytes
     for (int j = 0; j < 4; j++) {
-        //ground latchPin and hold low for as long as you are transmitting
+        // Recuperamos el byte a enviar
         data = dataArray[j];
+        // Ponemos en bajo el latch del registro para que no elimine 
+        // los datos que apenas esta cargando
         digitalWrite(latchPin, LOW);
+        // Enviamos el datos de forma serial comenzando por el bit 
+        // mas significativo
         shiftOut(dataPin, clockPin, MSBFIRST, j);
-        //return the latch pin high to signal chip that it
-        //no longer needs to listen for information
+        // Regresamos a alto el latch del registro dado que ya no esta 
+        // escuchando
         digitalWrite(latchPin, HIGH);
+        // Delay para dar tiempo de respuesta al circuito
         delay(10);
     }
+    // Desactivamos la limpieza de los FF
     digitalWrite(clrFF, LOW);
+    // Ponemos el reloj en cero
     digitalWrite(dCLK, LOW);
 }
 
 void loop() {
+    // Switcheamos el bit del reloj cada segundo
     digitalWrite(dCLK, HIGH);
     delay(1000);
     digitalWrite(dCLK, LOW);
     delay(1000);
 }
-
-
-
